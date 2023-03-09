2f4e07ba36f32b2fe013d43c83fa42b06d71ba5d
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2023-03-09 12:52:49 -0700

    Add joi for env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './modules/database/database.module';
+import * as Joi from '@hapi/joi';
 
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             cache: true,
-            isGlobal: true
+            isGlobal: true,
+            validationSchema: Joi.object({
+                POSTGRES_HOST: Joi.string().required(),
+                POSTGRES_PORT: Joi.number().required(),
+                POSTGRES_USER: Joi.string().required(),
+                POSTGRES_PASSWORD: Joi.string().required(),
+                POSTGRES_DIALECT: Joi.string().required(),
+                POSTGRES_DB: Joi.string().required(),
+                NODE_ENV: Joi.string().required(),
+            })
         }),
         DatabaseModule
     ],
